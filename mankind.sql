62aecfb55f6b64016e870161aa2de5a30e943de7
Mankind <340200600@qq.com>
2017-05-09 19:24:31 +0800

    explain

---
+++ mankind.sql
@@ -0,0 +1,125 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50635
+Source Host           : localhost:3306
+Source Database       : mankind
+
+Target Server Type    : MYSQL
+Target Server Version : 50635
+File Encoding         : 65001
+
+Date: 2017-05-09 19:16:01
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `blog`
+-- ----------------------------
+DROP TABLE IF EXISTS `blog`;
+CREATE TABLE `blog` (
+  `blog_id` int(11) NOT NULL AUTO_INCREMENT,
+  `blog_title` varchar(100) NOT NULL,
+  `blog_text` text NOT NULL,
+  `blog_visit` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `blog_date` date DEFAULT NULL,
+  `blog_pv` int(11) DEFAULT NULL,
+  PRIMARY KEY (`blog_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `blog_join_label`
+-- ----------------------------
+DROP TABLE IF EXISTS `blog_join_label`;
+CREATE TABLE `blog_join_label` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `blog_id` int(11) DEFAULT NULL,
+  `label_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- ----------------------------
+-- Table structure for `comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `father_id` int(11) NOT NULL,
+  `blog_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `content` text,
+  `date` datetime DEFAULT NULL,
+  `have_reply` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `fans`
+-- ----------------------------
+DROP TABLE IF EXISTS `fans`;
+CREATE TABLE `fans` (
+  `user_id` int(11) NOT NULL,
+  `noticer_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `label`
+-- ----------------------------
+DROP TABLE IF EXISTS `label`;
+CREATE TABLE `label` (
+  `label_id` int(11) NOT NULL AUTO_INCREMENT,
+  `label_title` varchar(30) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`label_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `place`
+-- ----------------------------
+DROP TABLE IF EXISTS `place`;
+CREATE TABLE `place` (
+  `place_id` int(11) NOT NULL AUTO_INCREMENT,
+  `province` varchar(10) DEFAULT NULL,
+  `city` varchar(10) DEFAULT NULL,
+  `area` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`place_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_account` varchar(20) DEFAULT NULL,
+  `user_password` char(32) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `userinfo`
+-- ----------------------------
+DROP TABLE IF EXISTS `userinfo`;
+CREATE TABLE `userinfo` (
+  `userinfo_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `user_name` varchar(20) DEFAULT NULL,
+  `user_sex` tinyint(1) DEFAULT NULL,
+  `user_birthday` date DEFAULT NULL,
+  `user_job` varchar(20) DEFAULT NULL,
+  `user_post` varchar(50) DEFAULT NULL,
+  `user_introduction` text,
+  `user_place` int(11) DEFAULT NULL,
+  `user_image` varchar(50) DEFAULT NULL,
+  `user_recent_login_time` datetime DEFAULT NULL,
+  `user_PV` int(11) DEFAULT NULL,
+  PRIMARY KEY (`userinfo_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
